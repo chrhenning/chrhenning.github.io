704229710f9978172cf75b5a67a4632b760d09bc
chrhenning <chrhenning@web.de>
2026-05-04 03:32:34 +0000

    Deploying to gh-pages from @ chrhenning/chrhenning.github.io@466ed020ede7cedc370609f0eedc06d854105139 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,11 +63,11 @@ ninja.data = [{
             window.location.href = "/blog/2026/code-sharing-in-monorepos/";
           
         },
-      },{id: "post-ethon-why-your-next-visual-quality-inspection-system-will-be-training-free",
+      },{id: "post-just-a-moment",
         
-          title: 'Ethon: Why your next visual quality inspection system will be training-free <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: 'Just a moment... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
-        description: "Visual inspection should not require long setup or labeling. One example should be enough to deliver reliable results. Here’s how.",
+        description: "",
         section: "Posts",
         handler: () => {
           
@@ -151,11 +151,11 @@ ninja.data = [{
             window.location.href = "/blog/2025/on-anthropomorphizing-intermediate-tokens/";
           
         },
-      },{id: "post-ethon-industrial-anomaly-detection-using-only-defect-free-images-to-train-your-inspection-model",
+      },{id: "post-just-a-moment",
         
-          title: 'Ethon: Industrial anomaly detection: Using only defect-free images to train your inspection model... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: 'Just a moment... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
-        description: "Why visual inspection should rely on approaches that do not require images of defective products for training.",
+        description: "",
         section: "Posts",
         handler: () => {
           
